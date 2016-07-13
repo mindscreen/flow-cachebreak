0572c36b205dfc1c5d100ad7a9eb68e61bb5ee2a
heilmann <heilmann@mindscreen.de>
2016-07-13 17:28:49 +0200

    Update documentation

--- Classes/ViewHelpers/Uri/ResourceViewHelper.php
+++ Classes/ViewHelpers/Uri/ResourceViewHelper.php
@@ -13,39 +13,39 @@ use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Flow\Resource\Resource;
 
 /**
- * A view helper for creating URIs to resources.
+ * A view helper for creating URIs to resources (including a cache breaking GET parameter).
  *
  * = Examples =
  *
  * <code title="Defaults">
- * <link href="{f:uri.resource(path: 'CSS/Stylesheet.css')}" rel="stylesheet" />
+ * <link href="{cb:uri.resource(path: 'CSS/Stylesheet.css')}" rel="stylesheet" />
  * </code>
  * <output>
- * <link href="http://yourdomain.tld/_Resources/Static/YourPackage/CSS/Stylesheet.css" rel="stylesheet" />
+ * <link href="http://yourdomain.tld/_Resources/Static/YourPackage/CSS/Stylesheet.css?cachebreakingstring" rel="stylesheet" />
  * (depending on current package)
  * </output>
  *
  * <code title="Other package resource">
- * {f:uri.resource(path: 'gfx/SomeImage.png', package: 'DifferentPackage')}
+ * {cb:uri.resource(path: 'gfx/SomeImage.png', package: 'DifferentPackage')}
  * </code>
  * <output>
- * http://yourdomain.tld/_Resources/Static/DifferentPackage/gfx/SomeImage.png
+ * http://yourdomain.tld/_Resources/Static/DifferentPackage/gfx/SomeImage.png?cachebreakingstring
  * (depending on domain)
  * </output>
  *
  * <code title="Resource URI">
- * {f:uri.resource(path: 'resource://DifferentPackage/Public/gfx/SomeImage.png')}
+ * {cb:uri.resource(path: 'resource://DifferentPackage/Public/gfx/SomeImage.png')}
  * </code>
  * <output>
- * http://yourdomain.tld/_Resources/Static/DifferentPackage/gfx/SomeImage.png
+ * http://yourdomain.tld/_Resources/Static/DifferentPackage/gfx/SomeImage.png?cachebreakingstring
  * (depending on domain)
  * </output>
  *
  * <code title="Resource object">
- * <img src="{f:uri.resource(resource: myImage.resource)}" />
+ * <img src="{cb:uri.resource(resource: myImage.resource)}" />
  * </code>
  * <output>
- * <img src="http://yourdomain.tld/_Resources/Persistent/69e73da3ce0ad08c717b7b9f1c759182d6650944.jpg" />
+ * <img src="http://yourdomain.tld/_Resources/Persistent/69e73da3ce0ad08c717b7b9f1c759182d6650944.jpg?cachebreakingstring" />
  * (depending on your resource object)
  * </output>
  *
